4a1bd14679c8e77936933883f8ccbc1eb5e9a9cb
LAPTOP-QSGRN4CF\TonyTaze <tonytaze096@gmail.com>
2017-10-07 16:09:07 -0600

    Interfaz

--- SDES-Algorithm/SDES-Algorithm/Program.cs
+++ SDES-Algorithm/SDES-Algorithm/Program.cs
@@ -10,11 +10,64 @@ namespace SDES_Algorithm
     {
         static void Main(string[] args)
         {
-            string name = "", password = "Hola";
+            string name = "", nemeInFile = "";
+            
+            string[] entrada = Console.ReadLine().Split(' ');
+            int current = CaracterValido(0, entrada);
 
-            SDESMethods Encryption = new SDESMethods("SDES", ref name, password);
-            var n = Encryption.Ciphertext("11100010");
-            var d = Encryption.Desencrypted(n,name);
+            switch (entrada[current])
+            {
+                case "-c":
+                    Encrypt(entrada, current, name);
+                    break;
+                case "-d":
+                    Decrypt(entrada, current, name, nemeInFile);
+                    break;
+                case "--ayuda":
+                    Console.WriteLine("Encriptar -c <palabra clave>\nDscomprimir-d\nRuta de archivo -f\nAyuda --ayuda");
+                    break;
+                default:
+                    Console.WriteLine("Comando no válido.\nEscriba --ayuda para obtener ayuda");
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        static public int CaracterValido(int posicion, string[] entrada)
+        {
+            if (entrada[posicion] == "")
+                return CaracterValido(posicion + 1, entrada);
+            return posicion;
+        }
+
+        static public void Encrypt(string[] entrada, int current, string name)
+        {
+            string password;
+            var eightBits = "";
+            current = CaracterValido(current++, entrada);
+            password = entrada[current];
+            if (password == null)
+                Console.WriteLine("Sintaxis no válida\nEscriba --ayuda para obtener ayuda");
+            else
+            {
+                SDESMethods Encryption = new SDESMethods("SDES", ref name, password);
+                Encryption.Ciphertext(eightBits);
+            }
+        }
+
+        static public void Decrypt(string[] entrada, int current, string name, string nameInFile)
+        {
+            string password;
+            var eightBits = "";
+            current = CaracterValido(current++, entrada);
+            password = entrada[current];
+            if(password == null)
+                Console.WriteLine("Sintaxis no válida\nEscriba --ayuda para obtener ayuda");
+            else
+            {
+                SDESMethods Encryption = new SDESMethods("SDES", ref name, password);
+                Encryption.Desencrypted(eightBits, nameInFile);
+            }
         }
     }
 }
