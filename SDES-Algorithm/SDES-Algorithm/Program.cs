fd34bf9730f4cc665086943acf09108e61107e1f
katherine Lorenzo <krle_10@hotmail.com>
2017-10-07 12:05:38 -0600

    Merge now with keys

--- SDES-Algorithm/SDES-Algorithm/Program.cs
+++ SDES-Algorithm/SDES-Algorithm/Program.cs
@@ -15,6 +15,12 @@ namespace SDES_Algorithm
             SDESMethods Encryption = new SDESMethods("SDES", ref name, password);
             var n = Encryption.Ciphertext("11100010");
             var d = Encryption.Desencrypted(n,name);
+
+            n = Encryption.Ciphertext("11111111");
+            d = Encryption.Desencrypted(n, name);
+
+            n = Encryption.Ciphertext("00001111");
+            d = Encryption.Desencrypted(n, name);
         }
     }
 }
